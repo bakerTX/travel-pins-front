76b73ff78da05cce9f6ad37311084df9ba955f4a
Aaron Baker <aaronbaker9210@gmail.com>
2016-10-21 14:52:03 -0500

    form puts a pin on the map

--- js/map.js
+++ js/map.js
@@ -1,16 +1,32 @@
 var map;
-
+var geocoder;
 function initMap() {
+  geocoder = new google.maps.Geocoder();
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 39.8282, lng: -98.5795},
     zoom: 4,
     disableDefaultUI: true
   });
   map.setOptions({draggable: false, zoomControl: false, scrollwheel: false, disableDoubleClickZoom: true});
+
 }
 
 
+
+function initialize() {
+  console.log('hello?');
+
+  console.log('it should have ran by now');
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
+    zoom: 8,
+    center: latlng
+  }
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
 function codeAddress() {
+  console.log('inside codeAddress');
   var address = document.getElementById('address').value;
   geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == 'OK') {
@@ -24,3 +40,8 @@ function codeAddress() {
     }
   });
 }
+
+$('#new-pin').on('submit',function(e){
+  e.preventDefault();
+  codeAddress();
+})
