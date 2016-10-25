00e3bd71c994f9e539658f79b0633f1255d133df
Aaron Baker <aaronbaker9210@gmail.com>
2016-10-25 08:38:09 -0500

    fast search after removing some unnecessary loops

--- js/want.js
+++ js/want.js
@@ -1,39 +1,30 @@
 $(document).ready(function() {
   $('form').on('submit', function(e){
     e.preventDefault();
-    var query = $('#search').val();
+    var query = $('#search').val(); // query comes from the word typed into the search field
     $('#showPhoto').empty();
-    loadFlicks(query);
+    loadFlicks(query); // sending in query
   });
 });
 
 
-var loadFlicks = function(json){
+var loadFlicks = function(json){ // query == json?
+  console.log('load flicks is running');
   console.log(json);
-  var value= $('#cityLookup').val()
+  var value= $('#cityLookup').val() // value has become extinct
   $.ajax({
-    url: 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&format=json&name='+ value,
-    jsonpCallback: "logResults",
+    url: 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&format=json&name='+ value, // replace with json? replace with query?
+    // jsonpCallback: "logResults",
     dataType: 'jsonp',
     data: {
       api_key: 'bb51ffbd4a070baac4fcb1b8e1325a8e'
     }
   })
   .done(function(response){
-    for (var i=0; i<5; i++){
-      // var parseResponse = $(response).parseJSON();
-      console.log('response', response);
-      // var $img = $('<img />').attr('src', response[i]);
-      // var $p = $('<p>X</p>');
 
-
-
-      $('.content').append('#document');
-    }
   })
 
   .fail(function(jqXHR, textStatus, errorThrown) {
-    // alert('oh no!');
     console.log(textStatus, errorThrown);
   })
   .always(function() {
@@ -43,7 +34,7 @@ var loadFlicks = function(json){
 
 function jsonFlickrApi(jsonObject){
   var imageInfo = jsonObject.photos.photo
-  for (var i=0; i<=imageInfo.length; i++)
+  for (var i=0; i<=imageInfo.length; i++) {}
   // console.log(imageInfo[i]);
   // https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
   // var farm = imageInfo[i].farm
@@ -54,24 +45,24 @@ function jsonFlickrApi(jsonObject){
 
   $.each( jsonObject.photos.photo, function( i, gp ) {
 
-var farmId = gp.farm;
-var serverId = gp.server;
-var id = gp.id;
-var secret = gp.secret;
+  var farmId = gp.farm;
+  var serverId = gp.server;
+  var id = gp.id;
+  var secret = gp.secret;
 
-console.log(farmId + ", " + serverId + ", " + id + ", " + secret);
+  // console.log(farmId + ", " + serverId + ", " + id + ", " + secret);
 
-//  https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-var $li = $('<li />');
-$li.append('<img src="https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg"/>');
-$('#showPhoto').append($li);
+  //  https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+  var $li = $('<li />');
+  $li.append('<img src="https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg"/>');
+  $('#showPhoto').append($li);
 
-// });
-});
+  });
+};
 
   // console.log(imageInfo[i].farm + ", " + imageInfo[i].server + ", " + imageInfo[i].id + ", " + imageInfo[i].secret);
 
   // var imageUrl = "'https://' + imageInfo[i].farm + '.staticflickr.com/' + imageInfo[i].server + '/' + imageInfo[i].id + '_' + imageInfo[i].secret + '.jpg'"
   // $("#flickr").append('<img src="https://farm' + imageInfo[i].farm + '.staticflickr.com/' + imageInfo[i].server + '/' + imageInfo[i].id + '_' + imageInfo[i].secret+ '.jpg"/>');
-  console.log(imageUrl);
-}
+  // console.log(imageUrl);
+// }
