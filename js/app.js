0843ff90a139d3fe85d4ec9fffd690739acc18ff
Aaron Baker <aaronbaker9210@gmail.com>
2016-10-27 14:19:18 -0500

    a lil cleanup

--- js/app.js
+++ js/app.js
@@ -92,22 +92,22 @@ $(document).ready(function() {
   };
 
 
-function fillExamplePins() {
-  console.log('filling example pins');
-  console.log(map);
-  markerJackson();
-  markerScotland();
-  markerNYC();
-}
+  function fillExamplePins() {
+    console.log('filling example pins');
+    console.log(map);
+    markerJackson();
+    markerScotland();
+    markerNYC();
+  }
 
-function markerJackson() {
-  var infowindow = new google.maps.InfoWindow()
-  var markerJackson = new google.maps.Marker({
-    // Jackson WY
-    position: {lat: 43.4912, lng: -110.81347},
-    map: map,
-    infowindow: infowindow
-  })
+  function markerJackson() {
+    var infowindow = new google.maps.InfoWindow()
+    var markerJackson = new google.maps.Marker({
+      // Jackson WY
+      position: {lat: 43.4912, lng: -110.81347},
+      map: map,
+      infowindow: infowindow
+    })
   examplePins.push(markerJackson);
   google.maps.event.addListener(markerJackson, 'click', function(e) {
     var marker = e.currentTarget;
@@ -118,170 +118,125 @@ function markerJackson() {
     )
     infowindow.open(map, markerJackson)
   });
-}
+  }
 
-function markerScotland() {
-  var infowindow = new google.maps.InfoWindow()
-  var markerScotland = new google.maps.Marker({
-    // Scotland
-    position: {lat: 57.8918, lng: -4.3464},
-    map: map,
-    infowindow: infowindow
+  function markerScotland() {
+    var infowindow = new google.maps.InfoWindow()
+    var markerScotland = new google.maps.Marker({
+      // Scotland
+      position: {lat: 57.8918, lng: -4.3464},
+      map: map,
+      infowindow: infowindow
+    })
+    examplePins.push(markerScotland)
+    google.maps.event.addListener(markerScotland, 'click', function(e) {
+      var marker = e.currentTarget;
+      this.infowindow.setContent(
+        `City: Bonar Bridge, Scotland<br>
+        Date: January 19<br>
+        Journal: Locals can be quite test`
+      )
+      infowindow.open(map, markerScotland)
   })
-  examplePins.push(markerScotland)
-  google.maps.event.addListener(markerScotland, 'click', function(e) {
-    var marker = e.currentTarget;
-    this.infowindow.setContent(
-      `City: Bonar Bridge, Scotland<br>
-      Date: January 19<br>
-      Journal: Locals can be quite test`
-    )
-    infowindow.open(map, markerScotland)
-})
-}
+  }
 
-function markerNYC() {
-  var infowindow = new google.maps.InfoWindow()
-  var markerNYC = new google.maps.Marker({
-    // New York City
-    position: {lat: 40.7128, lng: -74.0059},
-    map: map,
-    infowindow: infowindow
-  })
-  examplePins.push(markerNYC);
-  google.maps.event.addListener(markerNYC, 'click', function(e) {
-    var marker = e.currentTarget;
-    this.infowindow.setContent(
-      `City: New York City, New York<br>
-      Date: Aug 2015<br>
-      Journal: Spent so much money`
-    )
-    infowindow.open(map, markerNYC)
-  });
-}
+  function markerNYC() {
+    var infowindow = new google.maps.InfoWindow()
+    var markerNYC = new google.maps.Marker({
+      // New York City
+      position: {lat: 40.7128, lng: -74.0059},
+      map: map,
+      infowindow: infowindow
+    })
+    examplePins.push(markerNYC);
+    google.maps.event.addListener(markerNYC, 'click', function(e) {
+      var marker = e.currentTarget;
+      this.infowindow.setContent(
+        `City: New York City, New York<br>
+        Date: Aug 2015<br>
+        Journal: Spent so much money`
+      )
+      infowindow.open(map, markerNYC)
+    });
+  }
 
-
-// google.maps.event.addListener(marker, 'click', function(e) {
-//   console.log(this);
-//   var index = this.index;
-//   const thismarker = e.currentTarget;
-//   $('#delete').click(function(thismarker) {
-//     console.log(thismarker);
-//     markers[index].setMap(null);
-//     });
-// })
-
-function clickNewPin() {
-  map.setOptions({draggableCursor:'crosshair'});
-  var listen = google.maps.event.addListener(map, 'click', geo);
-  function geo(event) {
-    map.setOptions({draggableCursor:'null'});
-    geocoder.geocode({
-      'latLng': event.latLng
-    }, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        if (results[0]) {
-          var lat = results[0].geometry.bounds.f.b;
-          var lon = results[0].geometry.bounds.b.b;
-          console.log(lat, lon);
-          var city = results[0].address_components[1].long_name + ', ' + results[0].address_components[3].short_name;
-          placeMarker(event.latLng, city, listen, lat, lon);
+  function clickNewPin() {
+    map.setOptions({draggableCursor:'crosshair'});
+    var listen = google.maps.event.addListener(map, 'click', geo);
+    function geo(event) {
+      map.setOptions({draggableCursor:'null'});
+      geocoder.geocode({
+        'latLng': event.latLng
+      }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          if (results[0]) {
+            var lat = results[0].geometry.bounds.f.b;
+            var lon = results[0].geometry.bounds.b.b;
+            console.log(lat, lon);
+            var city = results[0].address_components[1].long_name + ', ' + results[0].address_components[3].short_name;
+            placeMarker(event.latLng, city, listen, lat, lon);
+          }
         }
+      });
+    };
+  };
+
+  function placeMarker(location, address, listen, lat, lon) {
+    $('#new-pin').show();
+    var infowindow = new google.maps.InfoWindow();
+    var user = Lockr.get('user');
+    var custom_data = {
+      position: location,
+      address: address,
+      map: map,
+      user: user,
+      lat: lat,
+      lon: lon,
+      infowindow: infowindow
+    }
+    var marker = new google.maps.Marker(custom_data);
+
+    google.maps.event.removeListener(listen);
+
+    markers.push(marker)
+    $('#new-pin').on('submit', function() {
+
+      if (isSignedIn() == undefined){
+        alert('sign in first! :)');
+        lock.show();
       }
+      var journal = document.getElementById('journal').value;
+      var date = document.getElementById('date').value;
+      $(this).hide();
+      marker = markers[markers.length - 1];
+      marker.journal = journal;
+      marker.date = date;
+      console.log(marker);
+      custom_data.journal = journal;
+      custom_data.date = date;
+      console.log(marker);
+      ajaxPost(custom_data);
+
+      google.maps.event.addListener(marker, 'click', function() {
+         this.infowindow.setContent(
+         `City: ${this.location}<br>
+         Date: ${this.date}<br>
+         Journal: ${this.journal}<br>
+         <span id='delete'>Delete Pin</span>`);
+         infowindow.open(map, this);
+        });
+
+        google.maps.event.addListener(marker, 'click', function(e) {
+          console.log(this);
+          var index = this.index;
+          const thismarker = e.currentTarget;
+          $('#delete').click(function(thismarker) {
+            console.log(thismarker);
+            markers[index].setMap(null);
+          });
+        });
     });
   };
-};
-
-function placeMarker(location, address, listen, lat, lon) {
-  $('#new-pin').show();
-  var infowindow = new google.maps.InfoWindow();
-  var user = Lockr.get('user');
-  var custom_data = {
-    position: location,
-    address: address,
-    map: map,
-    user: user,
-    lat: lat,
-    lon: lon,
-    infowindow: infowindow
-  }
-  var marker = new google.maps.Marker(custom_data);
-
-  google.maps.event.removeListener(listen);
-
-  markers.push(marker)
-  $('#new-pin').on('submit', function() {
-
-    if (isSignedIn() == undefined){
-      alert('sign in first! :)');
-      lock.show();
-    }
-    var journal = document.getElementById('journal').value;
-    var date = document.getElementById('date').value;
-    $(this).hide();
-    marker = markers[markers.length - 1];
-    marker.journal = journal;
-    marker.date = date;
-    console.log(marker);
-    custom_data.journal = journal;
-    custom_data.date = date;
-    console.log(marker);
-    ajaxPost(custom_data);
-    fillPersonalPins();
-
-    google.maps.event.addListener(marker, 'click', function() {
-       this.infowindow.setContent(
-       `City: ${this.location}<br>
-       Date: ${this.date}<br>
-       Journal: ${this.journal}<br>
-       <span id='delete'>Delete Pin</span>`);
-       infowindow.open(map, this);
-      });
-
-      google.maps.event.addListener(marker, 'click', function(e) {
-        console.log(this);
-        var index = this.index;
-        const thismarker = e.currentTarget;
-        $('#delete').click(function(thismarker) {
-          console.log(thismarker);
-          markers[index].setMap(null);
-        });
-      });
-  });
-};
-// function fillPersonalPins(){
-//   console.log('filling personal pins');
-//   var options = {
-//     url: 'http://localhost:3000/pins',
-//     headers: {
-//       'Authorization': 'Bearer ' + localStorage.getItem('idToken')
-//     }
-//   }
-//   var request = $.ajax(options)
-//   request.done(function(response){
-//     console.log(response);
-//   })
-//   request.fail(function(jqXHR, textStatus, errorThrown){
-//     console.log('fuck');
-//   })
-// }
-
-  // function fillPersonalPins(){
-  //   console.log('filling personal pins');
-  //   var options = {
-  //     url: 'http://localhost:3000/pins',
-  //     headers: {
-  //       'Authorization': 'Bearer ' + localStorage.getItem('idToken')
-  //     }
-  //   }
-  //   var request = $.ajax(options)
-  //   request.done(function(response){
-  //     console.log(response);
-  //   })
-  //   request.fail(function(jqXHR, textStatus, errorThrown){
-  //     console.log('fuck');
-  //   })
-  // }
 
   function logOut() {
     localStorage.removeItem('idToken');
